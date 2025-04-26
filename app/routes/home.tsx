d2a6482e907b74e4982ffd8c58d7fe991f40d94b
Sergio Xalambrí <hello@sergiodxa.com>
2025-04-25 20:08:02 -0500

    refactor: clean up JSX structure in Home component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -19,21 +19,18 @@ export async function action({ request }: Route.ActionArgs) {
 
 export default function Home() {
   return (
-    <>
-      {/* <Welcome /> */}
-      <Form navigate={false} method="POST" className="p-10">
-        <div className="flex items-center justify-center gap-4">
-          <button type="submit" name="color-scheme" value="dark">
-            Dark
-          </button>
-          <button type="submit" name="color-scheme" value="light">
-            Light
-          </button>
-          <button type="submit" name="color-scheme" value="system">
-            System
-          </button>
-        </div>
-      </Form>
-    </>
+    <Form navigate={false} method="POST" className="p-10">
+      <div className="flex items-center justify-center gap-4">
+        <button type="submit" name="color-scheme" value="dark">
+          Dark
+        </button>
+        <button type="submit" name="color-scheme" value="light">
+          Light
+        </button>
+        <button type="submit" name="color-scheme" value="system">
+          System
+        </button>
+      </div>
+    </Form>
   );
 }
